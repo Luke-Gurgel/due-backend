323ed04057509815153da29f3a363387ccc11dfa
Luke Gurgel <luke.gurgel@track.com>
2019-10-14 22:37:54 -0400

    Add tests for user signup route

--- src/routes/user/__tests__/create.test.ts
+++ src/routes/user/__tests__/create.test.ts
@@ -3,19 +3,59 @@ import app from 'src/app'
 import User from 'src/models/user'
 import { userBody, clearDB } from './__fixtures__/db'
 
-beforeEach(clearDB)
+afterEach(clearDB)
+
+const signupRoute = '/users/signup'
 
 test('should sign up a new user, given valid request body', async () => {
   const res = await request(app)
-    .post('/users/signup')
+    .post(signupRoute)
     .send(userBody)
     .expect(201)
 
-  const createdUser = await User.findById(res.body.user._id)
+  const createdUser = await User.findOne({ email: userBody.email })
   expect(createdUser).not.toBeNull()
   createdUser && expect(createdUser.password).not.toBe(userBody.password)
-  createdUser && expect(res.body).toMatchObject({
-    user: { fullname: `${userBody.fname} ${userBody.lname}`, email: userBody.email },
+  createdUser && expect(res.body).toEqual({
     token: createdUser.tokens[0].token
   })
+})
+
+test('should not sign up a new user if email is invalid', async () => {
+  await request(app)
+    .post(signupRoute)
+    .send({ ...userBody, email: 'invalidemail.com' })
+    .expect(400)
+})
+
+test('should not sign up a new user if no name is provided', async () => {
+  await request(app)
+    .post(signupRoute)
+    .send({ email: 'test@example.com', password: 'IunnaIsFive' })
+    .expect(400)
+})
+
+test('should not sign up a new user if password is too short', async () => {
+  await request(app)
+    .post(signupRoute)
+    .send({ ...userBody, password: 'mimo' })
+    .expect(400)
+})
+
+test('should not sign up a new user if password contains the word password', async () => {
+  await request(app)
+    .post(signupRoute)
+    .send({ ...userBody, password: 'Mimopassword' })
+    .expect(400)
+})
+
+test('should not sign up user if another user with the same email already exists', async () => {
+  const user = new User(userBody)
+  await user.save()
+
+  await request(app)
+    .post(signupRoute)
+    .send(userBody)
+    .expect(400)
+
 })
--- src/routes/user/create.ts
+++ src/routes/user/create.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from 'express'
 import User from 'src/models/user'
 
-export const createUser = async (req: Request, res: Response): Promise<void> => {
+export const createUser = async (req: Request, res: Response): Promise<Response | void> => {
+  const userWithSameEmail = await User.findOne({ email: req.body.email })
+  if (userWithSameEmail) {
+    return res.status(400).send({ error: "User with same email already exists." })
+  }
+
   const user = new User(req.body)
 
   try {
     await user.save()
     const token = await user.generateJwt()
-    console.log('user', user);
-    res.status(201).send({ user, token })
+    res.status(201).send({ token })
   } catch (error) {
     res.status(400).send({ error })
   }
