5b1b35ccecc002f876fc3f63f8ac39cff511df26
Luke Gurgel <luke.gurgel@track.com>
2019-10-17 18:28:11 -0400

    Add route for fetching user profile

---
+++ src/routes/user/__tests__/profile.test.ts
@@ -0,0 +1,26 @@
+import request from 'supertest'
+import app from 'src/app'
+import { setupDB, userOne } from './__fixtures__/db'
+
+beforeEach(setupDB)
+
+const route = '/users/me'
+const authHeader = 'Authorization'
+
+test('should return user public profile', async () => {
+  const res = await request(app)
+    .get(route)
+    .set(authHeader, 'Bearer ' + userOne.tokens[0].token)
+    .expect(200)
+
+  expect(res.body.user).not.toBeNull()
+  expect(res.body.user.password).toBeUndefined()
+  expect(res.body.user.tokens).toBeUndefined()
+})
+
+test('should not return profile for unauthorized user', async () => {
+  await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+})
