3fec73a7e3c14f768caec4509cb9f6e850a9f8be
Lucas Gurgel <luke.gur110@gmail.com>
2019-10-20 22:32:14 -0400

    Add user avatar upload route (#18)

--- src/routes/user/__tests__/profile.test.ts
+++ src/routes/user/__tests__/profile.test.ts
@@ -1,26 +1,27 @@
 import request from 'supertest'
 import app from 'src/app'
-import { setupDB, userOne } from './__fixtures__/db'
+import { userOne, setupDB, clearDB } from './__fixtures__/db'
 
 beforeEach(setupDB)
+afterAll(clearDB)
 
 const route = '/users/me'
 const authHeader = 'Authorization'
 
 test('should return user public profile', async () => {
-  const res = await request(app)
-    .get(route)
-    .set(authHeader, 'Bearer ' + userOne.tokens[0].token)
-    .expect(200)
+	const res = await request(app)
+		.get(route)
+		.set(authHeader, 'Bearer ' + userOne.tokens[0].token)
+		.expect(200)
 
-  expect(res.body.user).not.toBeNull()
-  expect(res.body.user.password).toBeUndefined()
-  expect(res.body.user.tokens).toBeUndefined()
+	expect(res.body.user).not.toBeNull()
+	expect(res.body.user.password).toBeUndefined()
+	expect(res.body.user.tokens).toBeUndefined()
 })
 
 test('should not return profile for unauthorized user', async () => {
-  await request(app)
-    .get('/users/me')
-    .send()
-    .expect(401)
+	await request(app)
+		.get('/users/me')
+		.send()
+		.expect(401)
 })
