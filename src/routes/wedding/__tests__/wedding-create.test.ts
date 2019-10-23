b13fb31f26372aafd42df7545539c0f7f1a91f17
Luke Gurgel <luke.gurgel@track.com>
2019-10-22 22:47:39 -0400

    Add route for wedding creation

---
+++ src/routes/wedding/__tests__/wedding-create.test.ts
@@ -0,0 +1,45 @@
+import request from 'supertest'
+import app from 'src/app'
+import { DueEventStatus } from 'src/models/wedding'
+import { clearDB, setupDB, userOne, userOneId } from './__fixtures__/db'
+
+beforeEach(setupDB)
+afterEach(clearDB)
+
+const route = '/wedding/create'
+const authHeader = 'Authorization'
+
+test('should create and return a new wedding', async () => {
+	const res = await request(app)
+		.post(route)
+		.set(authHeader, 'Bearer ' + userOne.tokens[0].token)
+		.expect(200)
+
+	expect(res.body.wedding).not.toBeNull()
+})
+
+test('newly created wedding should be associated with a specific user', async () => {
+	const res = await request(app)
+		.post(route)
+		.set(authHeader, 'Bearer ' + userOne.tokens[0].token)
+		.expect(200)
+
+	if (!res.body.wedding) throw Error()
+	expect(userOneId.toHexString()).toEqual(res.body.wedding.ownerId)
+})
+
+test('newly created wedding should have inactive status by default', async () => {
+	const res = await request(app)
+		.post(route)
+		.set(authHeader, 'Bearer ' + userOne.tokens[0].token)
+		.expect(200)
+
+	if (!res.body.wedding) throw Error()
+	expect(res.body.wedding.status).toBe(DueEventStatus.INACTIVE)
+})
+
+test('should not create wedding if not authenticated', async () => {
+	await request(app)
+		.post(route)
+		.expect(401)
+})
