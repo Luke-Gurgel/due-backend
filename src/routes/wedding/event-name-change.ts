873b41e9d7d9a7e36bc07a7ecb9a31fac1bbb0d1
Luke Gurgel <luke.gurgel@track.com>
2019-10-31 07:33:53 -0400

    Add event name change route

---
+++ src/routes/wedding/event-name-change.ts
@@ -0,0 +1,29 @@
+import { Response } from 'express'
+import { EnhancedRequest } from 'src/middlewares'
+import { DueEventStatus } from 'src/models/wedding'
+
+export const changeEventName = async (
+	req: EnhancedRequest,
+	res: Response,
+): Promise<Response | void> => {
+	if (!req.wedding) {
+		return res.status(500).send({ error: 'Something went wrong on our end' })
+	}
+
+	if (!req.body.eventName) {
+		return res.status(400).send({ error: 'An event name is required' })
+	}
+
+	if (req.wedding.status === DueEventStatus.ACTIVE) {
+		return res.status(403).send({ error: 'The event name cannot be changed after activation.' })
+	}
+
+	req.wedding.eventName = req.body.eventName
+
+	try {
+		req.wedding.save()
+		res.status(200).end()
+	} catch (error) {
+		res.status(500).send({ error })
+	}
+}
--- src/routes/wedding/index.ts
+++ src/routes/wedding/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { getGuestVersion } from './guest-version'
 import { purchaseEvent } from './purchase-event'
-import { auth } from 'src/middlewares'
+import { changeEventName } from './event-name-change'
+import { auth, weddingAuth } from 'src/middlewares'
 
 const weddingRouter = Router()
 
 weddingRouter.route('/wedding/purchaseEvent').post(auth, purchaseEvent)
-
 weddingRouter.route('/wedding/guestAccess').get(auth, getGuestVersion)
+weddingRouter.route('/wedding/eventName').post(auth, weddingAuth, changeEventName)
 
 export default weddingRouter
