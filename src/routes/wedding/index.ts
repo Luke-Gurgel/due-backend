597515cbbd8d85e9a93acf7af4930faf1eeeeaf6
Lucas Gurgel <luke.gur110@gmail.com>
2019-11-01 18:10:53 -0400

    Add route for adding a guest (#28)

--- src/routes/wedding/index.ts
+++ src/routes/wedding/index.ts
@@ -3,13 +3,20 @@ import { getGuestVersion } from './guest-version'
 import { purchaseEvent } from './purchase-event'
 import { changeEventName } from './event-name-change'
 import { getGuestList } from './get-guest-list'
+import { addGuest } from './add-guest'
 import { auth, weddingAuth } from 'src/middlewares'
 
 const weddingRouter = Router()
 
 weddingRouter.route('/wedding/purchaseEvent').post(auth, purchaseEvent)
+
 weddingRouter.route('/wedding/guestAccess').get(auth, getGuestVersion)
+
 weddingRouter.route('/wedding/eventName').post(auth, weddingAuth, changeEventName)
-weddingRouter.route('/wedding/guestList').get(auth, weddingAuth, getGuestList)
+
+weddingRouter
+	.route('/wedding/guests')
+	.get(auth, weddingAuth, getGuestList)
+	.post(auth, weddingAuth, addGuest)
 
 export default weddingRouter
